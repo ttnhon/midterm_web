1fc251e78de00c1720d3cc317b4806e6f683491c
DESKTOP-VIIL54U\phiph <phiphuc27@gmail.com>
2018-04-21 23:59:29 +0700

    phát thảo chức năng tìm kiếm

--- Phone_Shoping/1.js
+++ Phone_Shoping/1.js
@@ -1,4 +1,7 @@
-﻿ $(function(){
-	 
-})  
- 
+﻿$("#searchbtn").on('click',function() {
+	$('div.tieude_page').css('display','none');
+	$('div[class *= col-lg-3][id!=iphone]').css('display','none');
+	var text = $('#searchtxt').text();
+
+	return false;
+})
