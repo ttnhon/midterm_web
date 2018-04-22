e33611eabd59b368e3c01d22c98ba19cc171903e
NVPIT\NVP <nvpit97@gmail.com>
2018-04-22 23:44:41 +0700

    Cập nhật giao diện đăng ký và đăng nhập (trang chủ và trang chi tiết)

--- Phone_Shoping/1.js
+++ Phone_Shoping/1.js
@@ -1,7 +1,25 @@
-﻿$("#searchbtn").on('click',function() {
-	$('div.tieude_page').css('display','none');
-	$('div[class *= col-lg-3][id!=iphone]').css('display','none');
-	var text = $('#searchtxt').text();
+﻿$(function(){
+	 $("#searchbtn").on('click',function() {
+		$('div.tieude_page').css('display','none');
+		$('div[class *= col-lg-3][id!=iphone]').css('display','none');
+		var text = $('#searchtxt').text();
 
-	return false;
-})
+		return false;
+	});
+	 $('.nav-dangnhap').click(function(){
+	 	$('.manhinh_dangky').removeClass('ra');
+	 	$('.manhinh_dangnhap').addClass('ra');
+	 	return false; 
+	 });
+	 $('.nav-dangky').click(function(){
+	 	$('.manhinh_dangnhap').removeClass('ra');
+	 	$('.manhinh_dangky').addClass('ra');
+	 	return false; 
+	 });
+	 $('.dong_dangnhap').click(function(){
+	 	$('.manhinh_dangnhap').removeClass('ra');
+	 	$('.manhinh_dangky').removeClass('ra');
+	 	return false; 
+	 });
+
+})  
