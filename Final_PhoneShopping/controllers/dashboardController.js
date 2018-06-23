0b46565eb6580e4cd7276c992806e5a6337c7762
DESKTOP-VIIL54U\phiph <phiphuc27@gmail.com>
2018-06-23 18:50:01 +0700

    thêm chức năng quản lí loại sp

--- Final_PhoneShopping/controllers/dashboardController.js
+++ Final_PhoneShopping/controllers/dashboardController.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var hansanxuaRepo = require('../repos/HangSanXuatRepo.js');
+var loaisanphamRepo = require('../repos/LoaiSanPhamRepo.js');
 
 var router = express.Router();
 
@@ -12,7 +13,51 @@ router.get('/products', (req, res) => {
 });
 
 router.get('/categories', (req, res) => {
-	
+	loaisanphamRepo.loadAll().then(rows => {
+        var vm = {
+            categories: rows,
+            type: "categories"
+        };
+        res.render('dashboard/dashboardCategories',vm);
+    });
+});
+
+router.get('/categories/add', (req, res) => {
+    var vm = {
+        showAlert: false
+    };
+    res.render('dashboard/addCategory', vm);
+});
+router.post('/categories/add', (req, res) => {
+   loaisanphamRepo.add(req.body).then(value => {
+        var vm = {
+            showAlert: true
+        };
+        res.render('dashboard/addCategory', vm);
+    }).catch(err => {
+        res.end('fail');
+    });
+});
+router.get('/categories/edit', (req, res) => {
+    var maLoai = req.query.id;
+    loaisanphamRepo.single(maLoai).then(row => {
+        var vm = {
+            category: row,
+            showAlert: false
+        };
+        res.render('dashboard/editCategory', vm);
+    });
+});
+router.post('/categories/edit', (req, res) => {
+    loaisanphamRepo.update(req.body).then(value => {
+        var vm = {
+            category: req.body,
+            showAlert: true
+        };
+        res.render('dashboard/editCategory', vm);
+    }).catch(err => {
+        res.end('fail');
+    });
 });
 
 router.get('/producers', (req, res) => {
